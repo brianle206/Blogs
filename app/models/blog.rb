403cbfa180836a64619ba54d8b1d8b6702ae16bb
Brian Le <brianle206@Gmail.com>
2015-12-04 16:27:59 -0800

    did rake db and added 5 users and 5 blogs 1 owners)

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ActiveRecord::Base
+	has_many :post
+	has_many :owner
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	# has_many :post, :message, :blog, :owner
+	validates :first_name, :last_name, presence: true, length:{minimum: 2}
+	validates :email, presence: true, uniqueness:{case_sensitive: false}, format:{with: EMAIL_REGEX}
+	before_save do 
+		self.email.downcase!
+		self.first_name.capitalize!
+		self.last_name.capitalize!
+	end
 end
